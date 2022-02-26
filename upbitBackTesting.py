d53aecf97794461a73e59a197f5d75e7c58dd4cd
stardino2 <stardino2@nate.com>
2022-02-27 06:32:34 +0900

    +

--- upbitBackTesting.py
+++ upbitBackTesting.py
@@ -11,8 +11,8 @@ from buySell2 import buySell2
 res = login()
 # print(res.json())
 
-origDf = pyupbit.get_ohlcv("KRW-TFUEL", interval="minutes60", count = 200)
-# origDf = pyupbit.get_ohlcv("KRW-TFUEL", interval="day", count = 60)
+origDf = pyupbit.get_ohlcv("KRW-bora", interval="minutes60", count = 50)
+# origDf = pyupbit.get_ohlcv("KRW-bora", interval="day", count = 60)
 heikinAshiDf = heikinAshi(origDf)
 heikinAshiDf2 = heikinAshi(origDf)
 
@@ -26,10 +26,10 @@ heikinAshiDf2['buyPrice'] = buySellDf2[0]
 heikinAshiDf2['sellPrice'] = buySellDf2[1]
 heikinAshiDf2['total'] = buySellDf2[2]
 heikinAshiDf2['direction'] = buySellDf2[3]
-heikinAshiDf.to_excel("TFUEL-HAresultM60.xlsx")
-heikinAshiDf2.to_excel("TFUEL-HAresultM60-2.xlsx")
+# heikinAshiDf.to_excel("bora-HAresultM60.xlsx")
+# heikinAshiDf2.to_excel("bora-HAresultM60-2.xlsx")
 
-origDf = pyupbit.get_ohlcv("KRW-TFUEL", interval="minutes240", count = 50)
+origDf = pyupbit.get_ohlcv("KRW-bora", interval="minutes240", count = 50)
 heikinAshiDf = heikinAshi(origDf)
 heikinAshiDf2 = heikinAshi(origDf)
 
@@ -46,5 +46,5 @@ heikinAshiDf2['direction'] = buySellDf2[3]
 
 # print(heikinAshiDf)
 # showFigure(heikinAshiDf)
-# heikinAshiDf.to_excel("TFUEL-HAresultM240.xlsx")
-# heikinAshiDf2.to_excel("TFUEL-HAresultM240-2.xlsx")
+# heikinAshiDf.to_excel("bora-HAresultM240.xlsx")
+# heikinAshiDf2.to_excel("bora-HAresultM240-2.xlsx")
